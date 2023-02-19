c3e26bfa2eeaed49345ab09ea2dbe1bb6c4181e9
stefifm <bruerastefania@gmail.com>
2023-02-19 19:26:30 -0300

    Add task and create elements using JS

--- script.js
+++ script.js
@@ -3,7 +3,19 @@ const btnForm = document.querySelector('[data-form-btn]')
 const createTask = (e) => {
   e.preventDefault()
   const inputForm = document.querySelector('[data-form-input]')
-  console.log(inputForm.value)
+  const inputValue = inputForm.value
+  const list = document.querySelector('[data-list]')
+  const task = document.createElement('li')
+  task.classList.add('card')
+  inputForm.value = ''
+  const content = `<div>
+      <i class="far fa-check-square icon"></i>
+      <span class="task">${inputValue}</span>
+    </div>
+    <i class="fas fa-trash-alt trashIcon icon"></i>`
+
+  list.appendChild(task)
+  task.innerHTML = content
 }
 
 btnForm.addEventListener('click', createTask)
